0dd162180e7077b4177cd1d21f7b7537aec68d0d
Fiinnkk <felipegabrielfinkvieira@gmail.com>
2026-09-21 19:00:45 -0300

    FIX: mini menu receitas

--- js/receitas.js
+++ js/receitas.js
@@ -22,6 +22,7 @@ function filtrar(categoria, botao) {
         }
         else if (secao.id === categoria) {
             secao.style.display = "block";
+            botoes.style.color = "white";
         }
         else {
             secao.style.display = "none";
