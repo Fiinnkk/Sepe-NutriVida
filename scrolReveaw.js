54741bf2c8363164f82f99d4711878df506055e6
Fiinnkk <felipegabrielfinkvieira@gmail.com>
2026-09-23 08:58:51 -0300

    Inicio scrollReveal

---
+++ scrolReveaw.js
@@ -0,0 +1,33 @@
+const configBase = {
+    duration: 700,
+    easing: "ease-in-out",
+    distance: "50px",
+    opacity: 0
+}
+
+ScrollReveal().reveal('.home .revelarBaixo', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.home2 h2, .home2 p', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.home3 h2, .home3 p, .home3 ul li', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
