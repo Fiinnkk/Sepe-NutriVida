c16adc9da4d1060dd1cd478c73dc6e62304ae8f7
Fiinnkk <felipegabrielfinkvieira@gmail.com>
2026-09-23 13:19:54 -0300

    FEAT: Animação entrada das paginas pronta

--- scrolReveaw.js
+++ scrolReveaw.js
@@ -5,6 +5,8 @@ const configBase = {
     opacity: 0
 }
 
+/* === Home === */
+
 ScrollReveal().reveal('.home .revelarBaixo', {
     ...configBase,
     origin: "bottom",
@@ -44,6 +46,130 @@ ScrollReveal().reveal('.Alice, .Felipe, .Luciana, .Maria, .Matheus, .Pedro', {
 ScrollReveal().reveal('.acoes h2, .acoes p, .acoes ul li, .acoes ul li p ', {
     ...configBase,
     origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+/* === Benefícios === */
+
+
+
+ScrollReveal().reveal('.beneficios h1, .beneficios p', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.part-dois .card-beneficio, .part-dois h2', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+/* === Alimentação === */
+
+
+ScrollReveal().reveal('.hero-section .hero-conteudo h1, .hero-section .hero-conteudo p', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+ScrollReveal().reveal(' .card', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+/* === IMC === */
+
+
+ScrollReveal().reveal('.imc-banner h1, .imc-banner ul li, .imc-banner h2', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.formula-imc h1, .formula-imc h2, .formula-imc .formula', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.imc fieldset, .imc fieldset label, .imc fieldset input', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+/* === Receitas === */
+
+
+ScrollReveal().reveal('.pagina ul li', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.filtros', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+/* === Contatos === */
+
+
+ScrollReveal().reveal('.ajuda h1, .ajuda p', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.formulario .contato-texto, #formulario .field', {
+    ...configBase,
+    origin: "bottom",
+    scale: 0.95,
+    interval: 200,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.info-card', {
+    ...configBase,
+    origin: "left",
     scale: 0.95,
     interval: 200,
     delay: 200
